f0c9f7928a65efb55162a25a1a189928a4b5538d
anwareset <trianwar@protonmail.com>
2021-12-08 07:21:03 +0700

    Add k8s for scale up readme

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, redirect, url_for, make_response, session, jsonify
+from flask_session import Session
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host= "redis", port=6379)
+
+app.secret_key = 'BAD_SECRET_KEY'
+app.config['SESSION_TYPE'] = 'redis'
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_USE_SIGNER'] = True
+app.config['SESSION_REDIS'] = Redis(host="redis", port=6379)
+app.config.from_object(__name__)
+Session(app)
 
 @app.route('/')
 def hello():
-    redis.incr('hits')
-    return jsonify({'ip': request.remote_addr},{'visit': '{0}' . format (redis.get( 'hits' ))}), 200
+    if 'count' in session:
+        session['count'] = session.get('count') + 1
+    else:
+        session['count'] = 1
+    return jsonify({'ip': request.remote_addr},{'visit': '{0}' . format(session.get( 'count' ))}), 200
 
 if __name__ == "__main__":
     app.run(host= "0.0.0.0", debug=False)
